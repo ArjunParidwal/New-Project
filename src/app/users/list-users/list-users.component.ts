bd005490f292c3c0b623b5db99553754ad3dce9b
arjun paridwal <arjundigiinfotech@gmail.com>
2021-11-10 01:57:28 +0530

    project

---
+++ src/app/users/list-users/list-users.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-list-users',
+  templateUrl : './list-users.component.html',
+  styleUrls: ['./list-users.component.css']
+})
+export class ListUsersComponent implements OnInit {
+
+  listUsers : any;
+  constructor(private userSevice: UserService) { }
+
+  ngOnInit(): void {
+  this.userSevice.listUsers().subscribe(data => {
+    this.listUsers = data;
+  });
+  }
+
+}
